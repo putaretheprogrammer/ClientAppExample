6991db236b64e5ff41803a7ee017e2423ce5fac0
Putare <60327078+putaretheprogrammer@users.noreply.github.com>
2020-11-20 18:00:24 -0500

    update

--- client-chat.py
+++ client-chat.py
@@ -1,24 +1,32 @@
 import time, socket, sys
  
-socket_server = socket.socket()
-server_host = socket.gethostname()
-ip = socket.gethostbyname(server_host)
-sport = 8080
+new_socket = socket.socket()
+host_name = socket.gethostname()
+s_ip = socket.gethostbyname(host_name)
  
-print('This is your IP address: ',ip)
-server_host = input('Enter friend\'s IP address:')
-name = input('Enter Friend\'s name: ')
+port = 8080
+ 
+new_socket.bind((host_name, port))
+print( "Binding successful!”)
+print("This is your IP: ", s_ip)
+ 
+name = input('Enter name: ')
+ 
+new_socket.listen(1) 
  
  
-socket_server.connect((server_host, sport))
+conn, add = new_socket.accept()
  
-socket_server.send(name.encode())
-server_name = socket_server.recv(1024)
-server_name = server_name.decode()
+print("Received connection from ", add[0])
+print('Connection Established. Connected From: ',add[0])
  
-print(server_name,' has joined...')
+client = (conn.recv(1024)).decode()
+print(client + ' has connected.')
+ 
+conn.send(name.encode())
 while True:
-    message = (socket_server.recv(1024)).decode()
-    print(server_name, ":", message)
-    message = input("Me : ")
-    socket_server.send(message.encode())  
+    message = input('Me : ')
+    conn.send(message.encode())
+    message = conn.recv(1024)
+    message = message.decode()
+    print(client, ':', message)
